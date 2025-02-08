14ed093e2bb7c06ad8a2fecee497d574767a792e
NeilAbsurdist <npritchett@mail.pima.edu>
2025-02-07 22:25:39 -0700

    Update cis129_lab02_shortStory.py
    Updated file to include a short story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,9 @@
-print('Hello, World!')
+# Author: Neil Pritchett
+# My first Python lab in CIS129
+# This program prints a short story
+
+print("Once upon a time, a programmer wrote their first Python script.")
+print("They were nervous, but they followed the instructions carefully.")
+print("After running the script, 'Hello, World!' appeared on the screen.")
+print("Excited, they decided to add more code and make a story.")
+print("And so, their journey in programming began!")
